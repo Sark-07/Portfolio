11ac380dc07a73804158f47e4a9beba0f30d8577
sark-07 <sarkarpritam0007@gmail.com>
2023-10-20 11:18:20 +0530

    Removed falsy links

--- src/pages/FreelanceWork.jsx
+++ src/pages/FreelanceWork.jsx
@@ -5,13 +5,13 @@ const FreelanceWork = () => {
     <section id='freelance-work' className='freelance-work common-section-head'>
       <div className='common-section-head-inner'>
         <h2>Freelance Works</h2>
-        <p>Here are some of my cover designs!</p>
+        <p>Here are some of my cover designs</p>
       </div>
       <div className="freelance-work-img-grid-box">
         <ul className='freelance-work-img-grid'>
             {
                 bookCovers.map((items, index) => {
-                    return <li key={index} ><img src={items} alt={index} /></li>
+                    return <li key={index} ><a href={items} target='_blank'><img src={items} alt={index} /></a></li>
                 })
             }
         </ul>
